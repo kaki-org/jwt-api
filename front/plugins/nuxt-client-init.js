7dfb394f8b763768f6baac20cc52f5687db55d94
kakikubo <kakikubo@gmail.com>
2025-08-11 05:52:46 +0900

    feat: Nuxt 4.x移行 - プラグインのTypeScript化と認証システムの更新
    - auth.jsをTypeScript化し、新しいComposables + Piniaベースの認証システムに対応
    - nuxt-client-init.jsをTypeScript化し、新しい認証ストアを使用するよう更新
    - 後方互換性を保ちながら、新しいuseAuth() composableの使用を推奨
    - 型安全性の向上とモダンなNuxt 3パターンへの移行

--- front/plugins/nuxt-client-init.js
+++
@@ -1,9 +0,0 @@
-export default async ({ $auth, $axios }) => {
-  await $axios
-    .$post(
-      "/api/v1/auth_token/refresh",
-      {},
-      { validateStatus: (status) => $auth.resolveUnauthorized(status) }
-    )
-    .then((response) => $auth.login(response));
-};
